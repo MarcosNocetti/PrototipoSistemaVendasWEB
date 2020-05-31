9f7b9979f25a7eed71cb883c3561a986a93cf52f
Marcos Nocetti <nocettimarcos@gmail.com>
2020-05-31 06:14:18 -0300

    Update compra.php

--- assets/classes/compra.php
+++ assets/classes/compra.php
@@ -30,12 +30,21 @@ require('config.php');
     /* classe compra, responsavel por fazer operações no carrinho */    
     class compra{
         /* função para adionar itens no carrinho /OBS: guardar nomes dos itens e valor para utilizar nas outras funções/ */
-        public function addCarrinho(){
-
+        public function addCarrinho($id){
+            $sql = 'SELECT * FROM `tb_site.anuncios` WHERE id = ?';
+            $sql = $this->pdo->prepare($sql);
+            $sql->execute(array($id));
+    
+            if($sql->rowCount() > 0){
+                /* retornar o valor nome do item com o id selecionado e preço no carrinho */
+                return true;
+            }else{
+                return false;
+            }
         }
         /* soma o valor total para mostrar ao cliente quanto ele esta gastando ate o momento */
         public function valorTotal(){
-
+            
         } 
         /* função para dar um delete dos itens selecionados pelo usuario */
         public function retirarCarrinho(){
